8a04a625db7752c511b555ba3a5b6dab91f1c27d
Paul Soule <paul@souletech.com>
2018-07-06 18:04:02 +0100

    refactoring

--- BusinessIndex/appconfig.py
+++ BusinessIndex/appconfig.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Hello, my name is Rudolf and I have a secret...'
-    ITEMS_PER_PAGE = 20
-    MAX_ITEMS_RETURNED = 2000
+    ITEMS_PER_PAGE = 25
+    MAX_ITEMS_RETURNED = 5000
 
