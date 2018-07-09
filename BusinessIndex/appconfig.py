c926101ea72cdbcffdbdd4f51f77ba5ffbca78d2
Paul Soule <paul@souletech.com>
2018-07-09 11:28:19 +0100

    ongoing

--- BusinessIndex/appconfig.py
+++ BusinessIndex/appconfig.py
@@ -5,4 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Hello, my name is Rudolf and I have a secret...'
     ITEMS_PER_PAGE = 25
     MAX_ITEMS_RETURNED = 5000
+    INDEX = 'bi'
 
--- BusinessIndex/search.py
+++ BusinessIndex/search.py
@@ -1,4 +1,3 @@
-
 from elasticsearch import Elasticsearch
 from appconfig import Config
 import json
@@ -7,20 +6,21 @@ es = Elasticsearch(['http://localhost:9200'])
 
 
 def search(search_string, page_no):
-
     start = page_no * Config.ITEMS_PER_PAGE - Config.ITEMS_PER_PAGE
+
+    search_string = check_reserved(search_string)
 
     query = json.dumps({
         "from": start, "size": Config.ITEMS_PER_PAGE,
         "query": {
-            "query_string": {
+            "simple_query_string": {
                 "query": search_string,
-                "fields": ["_all"]
+
             }
         }
     })
 
-    res = es.search(index="test", body=query)
+    res = es.search(index=Config.INDEX, body=query)
     res['hits']['items_found'] = res['hits']['total']
 
     if res['hits']['total'] > Config.MAX_ITEMS_RETURNED:
@@ -31,6 +31,13 @@ def search(search_string, page_no):
     else:
         res['hits']['paging_active'] = False
 
-
     return res['hits']
 
+
+def check_reserved(string):
+    # check for and escape
+    reserved = ['"', '\', ' + ', "'""]
+    for tag in reserved:
+        string = string.replace(tag, '\\' + tag)
+    print('Search String: ' + string)
+    return string
