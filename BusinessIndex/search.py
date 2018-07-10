305f577eba372aa4113debe709e3ef9cbac3bb44
Paul Soule <paul@souletech.com>
2018-07-10 15:23:44 +0100

    home time

--- BusinessIndex/search.py
+++ BusinessIndex/search.py
@@ -14,47 +14,37 @@ def search_name(search_string, page_no):
 
 
 def search_all(search_string, page_no):
-    start = page_no * Config.ITEMS_PER_PAGE - Config.ITEMS_PER_PAGE
-
-    search_string = check_reserved_characters(search_string)
-
-    query = json.dumps({
-        "from": start, "size": Config.ITEMS_PER_PAGE,
-        "query": {
-            "simple_query_string": {
-                "query": search_string,
-            }
-        }
-    })
-
-    res = es.search(index=Config.INDEX, body=query)
-    res['hits']['items_found'] = res['hits']['total']
-
-    if res['hits']['total'] > Config.MAX_ITEMS_RETURNED:
-        res['hits']['total'] = Config.MAX_ITEMS_RETURNED
-
-    if res['hits']['total'] > Config.ITEMS_PER_PAGE:
-        res['hits']['paging_active'] = True
-    else:
-        res['hits']['paging_active'] = False
-
-    return res['hits']
+    return search_field(search_string, 'ALL', page_no)
 
 
 def search_field(search_string, field, page_no):
     start = page_no * Config.ITEMS_PER_PAGE - Config.ITEMS_PER_PAGE
 
+    orig_search_string = search_string
     search_string = check_reserved_characters(search_string)
 
-    query = json.dumps({
-        "from": start, "size": Config.ITEMS_PER_PAGE,
-        "query": {
-            "simple_query_string": {
-                "query": search_string,
-                "fields": [field],
+    if field == 'ALL':
+
+        query = json.dumps({
+            "from": start, "size": Config.ITEMS_PER_PAGE,
+            "query": {
+                "simple_query_string": {
+                    "query": search_string,
+                }
             }
-        }
-    })
+        })
+
+    else:
+
+        query = json.dumps({
+            "from": start, "size": Config.ITEMS_PER_PAGE,
+            "query": {
+                "simple_query_string": {
+                    "query": search_string,
+                    "fields": [field],
+                }
+            }
+        })
 
     res = es.search(index=Config.INDEX, body=query)
     res['hits']['items_found'] = res['hits']['total']
@@ -66,6 +56,8 @@ def search_field(search_string, field, page_no):
         res['hits']['paging_active'] = True
     else:
         res['hits']['paging_active'] = False
+
+    res['hits']['search_string'] = orig_search_string
 
     return res['hits']
 
